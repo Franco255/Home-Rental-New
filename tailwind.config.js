faab22f035b4f99a305ccd492b25aee55bd20e2f
Franco255 <fmwasyola@yahoo.com>
2023-02-28 20:07:55 +0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        brightRed: 'hsla(14, 91%, 54%, 1)',
+        brightGrey: 'hsla(0, 0%, 96%, 1)',
+        lightRed: 'hsla(14, 91%, 54%, 0.07)',
+      },
+    },
+  },
+  plugins: [],
+}
